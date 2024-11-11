74996b73c28156464272b76f602ebfd174d9fc62
gusparck <araujorodrigues.guxtavo@gmail.com>
2024-11-11 17:02:19 -0300

    Melhorias par percentualGordura.java

--- src/percentualGordura.java
+++ src/percentualGordura.java
@@ -8,11 +8,11 @@ public  class percentualGordura {
         if(peso <= 0 || altura <= 0 || idade <= 0){
             System.out.println("Valores negativos são inválidos. Tente novamente.");
         } else{
-            if(sexo.equalsIgnoreCase("Masculino")){
+            if(sexo.equalsIgnoreCase("Masculino") || sexo.equalsIgnoreCase("M") || sexo.equalsIgnoreCase("Homem")){
                 s = 1;
                 resultado = ((1.20 * useIMC) + (0.23 * idade)-(10.8 * s) - 5.4);
                 System.out.printf("Seu percentual de gordura é: %.2f%%.%n", resultado);
-            } else if (sexo.equalsIgnoreCase("Feminino")){
+            } else if(sexo.equalsIgnoreCase("Feminino") || sexo.equalsIgnoreCase("F") || sexo.equalsIgnoreCase("Mulher")){
                 s = 0;
                 resultado = ((1.20 * useIMC) + (0.23 * idade)-(10.8 * s) - 5.4);
                 System.out.printf("Seu percentual de gordura é: %.2f%%.%n", resultado);
